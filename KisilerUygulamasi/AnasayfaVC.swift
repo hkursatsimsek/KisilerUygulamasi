cb326f4b0ea77bcdc53ecebeebc745867f921e4f
hkursatsimsek <kursat.simsek@modanisa.com>
2021-12-18 15:35:00 +0300

    Project started

---
+++ KisilerUygulamasi/AnasayfaVC.swift
@@ -0,0 +1,18 @@
+//
+//  AnasayfaVC.swift
+//  KisilerUygulamasi
+//
+//  Created by Kürşat Şimşek on 18.12.2021.
+//
+
+import UIKit
+
+class AnasayfaVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
