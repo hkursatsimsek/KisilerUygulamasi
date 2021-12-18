e48e286bf3126bc340a0ceceeefff3840dc3822e
hkursatsimsek <kursat.simsek@modanisa.com>
2021-12-18 16:48:44 +0300

    KisiDetayVC konfigüre edildi

--- KisilerUygulamasi/KisiDetayVC.swift
+++ KisilerUygulamasi/KisiDetayVC.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class KisiDetayVC: UIViewController {
+    @IBOutlet weak var tfKisiAd: UITextField!
+    @IBOutlet weak var tfKisiTel: UITextField!
     
     var kisi:Kisiler?
 
@@ -15,9 +17,21 @@ class KisiDetayVC: UIViewController {
         super.viewDidLoad()
 
         if let k = kisi {
-            print("Kişi : \(k.kisi_id!) - \(k.kisi_ad!) - \(k.kisi_tel!)")
+//            print("Kişi : \(k.kisi_id!) - \(k.kisi_ad!) - \(k.kisi_tel!)")
+            tfKisiAd.text = k.kisi_ad
+            tfKisiTel.text = k.kisi_tel
         }
         
+    }
+    
+    @IBAction func buttonGuncelle(_ sender: Any) {
+        if let kA = tfKisiAd.text, let kT = tfKisiTel.text, let k = kisi {
+            guncelle(kisi_id: k.kisi_id!, kisi_ad: kA, kisi_tel: kT)
+        }
+    }
+    
+    func guncelle(kisi_id:Int, kisi_ad:String, kisi_tel:String) {
+        print("Kişi güncelle : \(kisi_id) - \(kisi_ad) - \(kisi_tel)")
     }
     
 }
