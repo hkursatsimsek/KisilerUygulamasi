47cd7bc6bf5855222c8735c5b1f2f06150442834
hkursatsimsek <kursat.simsek@modanisa.com>
2021-12-18 15:43:24 +0300

    KisiKayit and NavigationController added.

---
+++ KisilerUygulamasi/KisiDetayVC.swift
@@ -0,0 +1,18 @@
+//
+//  KisiDetayVC.swift
+//  KisilerUygulamasi
+//
+//  Created by Kürşat Şimşek on 18.12.2021.
+//
+
+import UIKit
+
+class KisiDetayVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+}
---
+++ KisilerUygulamasi/KisiKayitVC.swift
@@ -0,0 +1,18 @@
+//
+//  KisiKayitVC.swift
+//  KisilerUygulamasi
+//
+//  Created by Kürşat Şimşek on 18.12.2021.
+//
+
+import UIKit
+
+class KisiKayitVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
