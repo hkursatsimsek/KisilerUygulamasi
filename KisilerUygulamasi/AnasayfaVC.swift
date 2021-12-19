9ede12522af44cff928bed07789bc6579551c679
hkursatsimsek <kursat.simsek@modanisa.com>
2021-12-19 15:33:00 +0300

    Anasayfa tableVeiw ve searchBar konfigürasyonları eklendi. Hücreden seçilen veri detay sayfasına gönderildi.

--- KisilerUygulamasi/AnasayfaVC.swift
+++ KisilerUygulamasi/AnasayfaVC.swift
@@ -8,16 +8,23 @@
 import UIKit
 
 class AnasayfaVC: UIViewController {
-
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var kisilerTableView: UITableView!
+    
+    var kisilerListe = [Kisiler]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        searchBar.delegate = self // Protokolü sayfaya bağlamamızı sağlar. DidLoad' da çağırmamız lazım extension' a almıyoruz.
         
-    }
-
-    @IBAction func denemeButton(_ sender: Any) {
-        let kisi = Kisiler(kisi_id: 1, kisi_ad: "Ahmet", kisi_tel: "11111")
-        performSegue(withIdentifier: "toDetay", sender: kisi)
+        kisilerTableView.delegate = self
+        kisilerTableView.dataSource = self
+        
+        let k1 = Kisiler(kisi_id: 1, kisi_ad: "Ahmet", kisi_tel: "11111")
+        let k2 = Kisiler(kisi_id: 2, kisi_ad: "Zeynep", kisi_tel: "22222")
+        kisilerListe.append(k1)
+        kisilerListe.append(k2)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -29,4 +36,32 @@ class AnasayfaVC: UIViewController {
         }
     }
     
+}
+
+extension AnasayfaVC: UISearchBarDelegate { // AnasayfaVC ' i genişletiyoruz
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print("Arama sonucu : \(searchText)")
+    }
+}
+
+extension AnasayfaVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return kisilerListe.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let kisi = kisilerListe[indexPath.row] // kişi nesnemizi aldık
+        let hucre = tableView.dequeueReusableCell(withIdentifier: "kisilerHucre",for: indexPath) as! TableViewHucre
+        
+        hucre.kisiBilgiLabel.text = "\(kisi.kisi_ad!) - \(kisi.kisi_tel!)"
+        
+        return hucre
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let kisi = kisilerListe[indexPath.row]
+        performSegue(withIdentifier: "toDetay", sender: kisi)
+//        print("Kişi : \(kisi.kisi_id!) - \(kisi.kisi_ad!) - \(kisi.kisi_tel!)")
+        tableView.deselectRow(at: indexPath, animated: true) // Satırın seçili görüntüsünü kaldırır.
+    }
 }
---
+++ KisilerUygulamasi/TableViewHucre.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewHucre.swift
+//  KisilerUygulamasi
+//
+//  Created by Kürşat Şimşek on 19.12.2021.
+//
+
+import UIKit
+
+class TableViewHucre: UITableViewCell {
+    @IBOutlet weak var kisiBilgiLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
